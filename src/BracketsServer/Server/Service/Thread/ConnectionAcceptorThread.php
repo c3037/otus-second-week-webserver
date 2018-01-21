5600ea2fa4457c94130d5fb8daac8578c07cde96
Dmitry P <dmitry.porozhnyakov@mail.ru>
2018-01-21 12:01:46 +0300

    renamed worker creator method

--- src/BracketsServer/Server/Service/Thread/ConnectionAcceptorThread.php
+++ src/BracketsServer/Server/Service/Thread/ConnectionAcceptorThread.php
@@ -115,7 +115,7 @@ final class ConnectionAcceptorThread extends Thread implements ThreadInterface
 
                 $workerPid = pcntl_fork();
                 if (empty($workerPid)) {
-                    $this->getWorkerPrototype()->communicate($newConnection);
+                    $this->getWorker()->communicate($newConnection);
                     break;
                 }
 
@@ -139,7 +139,7 @@ final class ConnectionAcceptorThread extends Thread implements ThreadInterface
      * @return WorkerInterface
      * @throws ContainerExceptionInterface
      */
-    private function getWorkerPrototype(): WorkerInterface
+    private function getWorker(): WorkerInterface
     {
         return $this->container->get('worker');
     }
